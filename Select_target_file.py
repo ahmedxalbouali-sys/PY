f433abf3adaeb4b31ab9bd24943b316c80df9de9
Ahmed <ahmedxalbouali@gmail.com>
2025-12-05 15:20:47 +0100

    bb

--- Select_target_file.py
+++ Select_target_file.py
@@ -49,8 +49,8 @@ def select_valid_file():
 
 #--- Example usage ---        
 
-selected = select_valid_file()
-if selected:
-    print("User selected:", selected)
-else:
-    print("User cancelled.")
+#selected = select_valid_file()
+#if selected:
+#    print("User selected:", selected)
+#else:
+#    print("User cancelled.")
